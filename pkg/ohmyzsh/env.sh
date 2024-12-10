5a60a7e8732389a792e747e5b27da6719ecb1921
Qingfei Kang <Qingfei.Kang@shopee.com>
2024-12-10 18:27:03 +0800

    [kdot] init structure

--- pkg/ohmyzsh/env.sh
+++ pkg/ohmyzsh/env.sh
@@ -1,1 +1,2 @@
 export ZDOTDIR=$XDG_CONFIG_HOME/zsh
+export ZSH=$XDG_CONFIG_HOME/zsh/oh-my-zsh
--- pkg/ohmyzsh/install.sh
+++ pkg/ohmyzsh/install.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-#
-# ZSH
 
 RED=$'\e[31m'
 GREEN=$'\e[32m'
@@ -9,8 +7,8 @@ RESET=$'\e[0m'
 
 echo "📦 Installing Oh-My-Zsh for you."
 
-if [[ ! -d "$HOME/.oh-my-zsh" ]]; then
-  git clone https://github.com/ohmyzsh/ohmyzsh.git $HOME/.oh-my-zsh || exit 1
+if [[ ! -d "$ZSH" ]]; then
+  git clone https://github.com/ohmyzsh/ohmyzsh.git $ZSH || exit 1
 
   if [[ $? == 1 ]]; then
     echo ${RED}✖${RESET} "Oh My Zsh"
@@ -21,43 +19,43 @@ else
   echo ${GREEN}✔${RESET} "Oh My Zsh"
 fi
 
-if [[ ! -d "$HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions" ]]; then
-  echo "安装 zsh-autosuggestions."
-  git clone https://github.com/zsh-users/zsh-autosuggestions.git $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions --depth=1 || exit 1
-
-  if [[ $? == 1 ]]; then
-    echo ${RED}✖${RESET} "zsh-autosuggestions"
-  else
-    echo ${GREEN}✔${RESET} "zsh-autosuggestions"
-  fi
-else
-  echo ${GREEN}✔${RESET} "zsh-autosuggestions"
-fi
-
-if [[ ! -d "$HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting" ]]; then
-  echo "安装 zsh-syntax-highlighting."
-  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting --depth=1 || exit 1
-
-  if [[ $? == 1 ]]; then
-    echo ${RED}✖${RESET} "zsh-syntax-highlighting"
-  else
-    echo ${GREEN}✔${RESET} "zsh-syntax-highlighting"
-  fi
-else
-  echo ${GREEN}✔${RESET} "zsh-syntax-highlighting"
-fi
-
-if [[ ! -d "$HOME/.oh-my-zsh/custom/plugins/zsh-completions" ]]; then
-  echo "安装 zsh-completions."
-  git clone https://github.com/zsh-users/zsh-completions $HOME/.oh-my-zsh/custom/plugins/zsh-completions --depth=1 || exit 1
-
-  if [[ $? == 1 ]]; then
-    echo ${RED}✖${RESET} "zsh-completions"
-  else
-    echo ${GREEN}✔${RESET} "zsh-completions"
-  fi
-else
-  echo ${GREEN}✔${RESET} "zsh-completions"
-fi
+#if [[ ! -d "$HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions" ]]; then
+#  echo "安装 zsh-autosuggestions."
+#  git clone https://github.com/zsh-users/zsh-autosuggestions.git $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions --depth=1 || exit 1
+#
+#  if [[ $? == 1 ]]; then
+#    echo ${RED}✖${RESET} "zsh-autosuggestions"
+#  else
+#    echo ${GREEN}✔${RESET} "zsh-autosuggestions"
+#  fi
+#else
+#  echo ${GREEN}✔${RESET} "zsh-autosuggestions"
+#fi
+#
+#if [[ ! -d "$HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting" ]]; then
+#  echo "安装 zsh-syntax-highlighting."
+#  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting --depth=1 || exit 1
+#
+#  if [[ $? == 1 ]]; then
+#    echo ${RED}✖${RESET} "zsh-syntax-highlighting"
+#  else
+#    echo ${GREEN}✔${RESET} "zsh-syntax-highlighting"
+#  fi
+#else
+#  echo ${GREEN}✔${RESET} "zsh-syntax-highlighting"
+#fi
+#
+#if [[ ! -d "$HOME/.oh-my-zsh/custom/plugins/zsh-completions" ]]; then
+#  echo "安装 zsh-completions."
+#  git clone https://github.com/zsh-users/zsh-completions $HOME/.oh-my-zsh/custom/plugins/zsh-completions --depth=1 || exit 1
+#
+#  if [[ $? == 1 ]]; then
+#    echo ${RED}✖${RESET} "zsh-completions"
+#  else
+#    echo ${GREEN}✔${RESET} "zsh-completions"
+#  fi
+#else
+#  echo ${GREEN}✔${RESET} "zsh-completions"
+#fi
 
 echo "📦 Installing Oh-My-Zsh done"
